afe34dca816c23ba3d0b35196315e63988f99ad0
Amr Hassan <amr_hassan522@yahoo.com>
2021-01-07 01:27:50 +0200

    edit congig

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
-app.use(express.static('./dist/hdya-front'));
+// app.use(express.static('./dist/HdyaFront'));
 
-app.get('*', function(req, res) {
-    res.sendFile('index.html', { root: 'dist/hdya-front/' });
+// app.get('*', function(req, res) {
+//     res.sendFile('index.html', { root: 'dist/HdyaFront/' });
+// });
+
+// app.listen(process.env.PORT || 8080);
+
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/HdyaFront'));
+
+app.get('/*', function(req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/HdyaFront/index.html'));
 });
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
